442e7770c2bf4588dbdcc1c522f38ccba71836cf
Dimitrios <jimly1209@yahoo.gr>
2021-04-04 21:04:12 +0300

    Upload_WithSomeErrors

--- gates_ver1.c
+++ gates_ver1.c
@@ -33,8 +33,8 @@
 /* User read-write, group read, others read */
 #define PERMS 0644
 
-int j = 0, w = 0;
-pid_t pidtable[100];
+int j = 0, w = 0, wstatus;
+pid_t pidtable[100], pid2, cpid;
 char buffer[100];
 bool gate_state[100];
 
@@ -118,10 +118,81 @@ void forker(int nprocesses) {
 
 void handle_dad_signal(int sig) {
   switch(sig) {
+
     case SIGUSR1:
-      for(int i = 0; i < j; i++) {
+      for(int i = 0; i < w; i++) {
         kill(pidtable[i], SIGUSR1);
-        //wait(pidtable[i]);
+        usleep(5000);
+      }
+      break;
+
+    case SIGUSR2:
+      for(int i = 0; i < w; i++) {
+        kill(pidtable[i], SIGUSR2);
+        usleep(5000);
+      }
+      break;
+
+
+    case SIGCHLD:
+      //pid_t cpid, pid2;
+      //int wstatus;
+
+      //printf("pid2=%d, cpid=%d, wstatus=%d\n", pid2, cpid, wstatus);
+      pid2 = waitpid(cpid, &wstatus, 0);
+      //printf("pid2=%d, cpid=%d, wstatus=%d\n", pid2, cpid, wstatus);
+
+      if (pid2 == -1) {
+        perror("waitpid");
+        exit(EXIT_FAILURE);
+      }
+
+      if (WIFEXITED(wstatus)) {                                 //CHILD IS TERMINATED
+        int count = 0;
+        bool check = true;
+        while(check) {
+          if(pidtable[count] != pid2) {
+            count++;
+          }
+          else {
+            check = false;
+          }
+        }
+        printf(MAGENTA "[PARENT/PID=%d] Child %d with PID=%d exited\n", getpid(), count, pid2);
+      
+        //CREATE NEW CHILD
+        cpid = fork();
+
+        if ((cpid) < 0) {
+          perror("fork");
+          exit(-1);
+        }
+
+        else if (cpid == 0) {
+          char str[3];
+          sprintf(str, "%d", count);
+          if(buffer[count] == 't') {
+            char *const argv[] = {"./child", str, "t", NULL};
+            int status = execv("./child", argv);
+            check_neg(status, "Failed to create child");
+          }
+          else {
+            char *const argv[] = {"./child", str, "f", NULL};
+            int status = execv("./child", argv);
+            check_neg(status, "Failed to create child");
+          }
+        }
+
+        else {     
+          if(buffer[count] == 't') {
+            printf(MAGENTA "[PARENT/PID=%d] Created new child for gate %d (PID=%d) and initial state 't'\n", getpid(), count, cpid);
+            fflush(stdout);         
+          }
+          else {
+            printf(MAGENTA "[PARENT/PID=%d] Created new child for gate %d (PID=%d) and initial state 'f'\n", getpid(), count, cpid);
+            fflush(stdout);  
+          } 
+        }
       }
       break;
   }
@@ -165,7 +236,28 @@ int main(int argc, char *argv[]) {
     perror("Error: cannot handle SIGTERM"); // Should not happen
   }
 
-  
+  if (sigaction(SIGCHLD, &sa, NULL) == -1) {
+    perror("Error: cannot handle SIGCHLD"); // Should not happen
+  }
+
+/*
+  do {
+    pid2 = waitpid(cpid, &wstatus, WUNTRACED | WCONTINUED);
+    printf("CHILDS PID %d\n", cpid);
+    if (pid2 == -1) {
+      perror("waitpid");
+      exit(EXIT_FAILURE);
+    }
+
+    if (WIFSTOPPED(wstatus)) {
+      printf(MAGENTA "stopped by signal %d\n", WSTOPSIG(wstatus));
+    } 
+    else if (WIFSIGNALED(wstatus)) {
+      printf(MAGENTA "killed by signal %d\n", WTERMSIG(wstatus));
+    }
+  } while (!WIFEXITED(wstatus) && !WIFSIGNALED(wstatus));
+*/
+
   while (1) {
     pause();
   }
